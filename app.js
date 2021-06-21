5980b85804e23569005a659b828bf0e846783529
Kelley Worthington <kelleymarne@gmail.com>
2021-06-21 16:12:20 -0600

    refactored the code to filter and map through feature projects etc

--- app.js
+++ app.js
@@ -134,19 +134,16 @@ const promptProject = portfolioData => {
     });
 };
 
+
+
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
 
-    // const pageHTML = generatePage(name, github);
+    fs.writeFile('index.html', pageHTML, err => {
+        if (err) throw new Error(err);
 
-// const [name, github] = profileDataArgs;
-
-
-// fs.writeFile('index.html', pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output!')
-// })
+        console.log('Portfolio complete! Check out index.html to see the output!')
+    })
 });
